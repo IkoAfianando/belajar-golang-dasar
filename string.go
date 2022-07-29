7ff885617f656b21ab3ee800d024fd4c73c380f3
IkoAfianando <ikoafianando123@gmail.com>
2022-07-29 11:02:57 +0700

    Initial Commit

---
+++ string.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Ini adalah program String")
+	fmt.Println(2, "Ini adalah program number")
+	fmt.Println("Iko Afianando")
+	fmt.Println("Iko")
+
+	fmt.Println(len("Iko"))
+	fmt.Println("Iko Afianando"[0])
+	fmt.Println("Iko Afianando"[1])
+}
