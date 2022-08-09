f5d4aac283a211b0f898f17416d56eded018c33e
IkoAfianando <ikoafianando123@gmail.com>
2022-08-09 19:19:12 +0700

    Inital commit

--- coding.js
+++ coding.js
@@ -6,6 +6,7 @@ class FizzBuzz {
     fizzBuzz(data) {
         for(let i = 1; i <= data; i++) {
             const data1 = i % 3 === 0 && i % 5 === 0;
+            const data2 = i % 3 === 0;
         }
     }
 }
